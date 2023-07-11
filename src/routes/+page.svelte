dd3fc2b4a348f4481a73bfa4f1cb31a0d4f22789
Samuel Middendorp <samiezaurus@gmail.com>
2023-07-11 15:31:27 +0200

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,88 +1,103 @@
 <script lang="ts">
     import { getBoard } from "$lib/FakeFileStore";
-    import { v4 as uuidv4 } from 'uuid';
+    import { v4 as uuidv4 } from "uuid";
 
     let board = getBoard();
 
-    let onDrop = (e: any, id: string) =>{
-        
-        // Leverage setting of event data 
+    let onDrop = (e: any, id: string) => {
+        // Leverage setting of event data
         let itemId: string = e.dataTransfer.getData("itemId");
         let currentLaneId: string = e.dataTransfer.getData("laneId");
 
         // When moving to itself, dont change
-        if(id == currentLaneId){
+        if (id == currentLaneId) {
             return;
         }
-        
+
         // Get indexes to make direct adjustment
-        let targetLaneIndex = board.lanes.indexOf(board.lanes.find(x => x.id == id)!); 
-        let currentLaneIndex = board.lanes.indexOf(board.lanes.find(x => x.id == currentLaneId)!); 
+        let targetLaneIndex = board.lanes.indexOf(
+            board.lanes.find((x) => x.id == id)!
+        );
+        let currentLaneIndex = board.lanes.indexOf(
+            board.lanes.find((x) => x.id == currentLaneId)!
+        );
 
         // Get lanes
         let targetLane = board.lanes[targetLaneIndex];
         let currentLane = board.lanes[currentLaneIndex];
-       
+
         // Get item
-        let item = board.lanes[currentLaneIndex].items.find(i => i.id == itemId)!;
+        let item = currentLane.items.find((i) => i.id == itemId)!;
 
         // Remove from existing and add to new;
-        targetLane.items = [...targetLane.items, item]; 
-        currentLane.items = currentLane.items.filter(i => i.id != itemId);
+        targetLane.items = [...targetLane.items, item];
+        currentLane.items = currentLane.items.filter((i) => i.id != itemId);
 
         // Update board
 
         board.lanes[targetLaneIndex] = targetLane;
         board.lanes[currentLaneIndex] = currentLane;
-        
-    }
+    };
 
-    let onDragOver = (e: any) =>{
+    let onDragOver = (e: any) => {
         e.preventDefault();
-    }
+    };
 
     let onDragStart = (e: any, itemId: string, laneId: string) => {
         // Set datatransfer when starting dragging of an item;
         e.dataTransfer.setData("itemId", itemId);
         e.dataTransfer.setData("laneId", laneId);
-    }
+    };
     let addLane = () => {
-        let newLane = {id: uuidv4(), title: "sample lane", items:[] }
+        let newLane = { id: uuidv4(), title: "sample lane", items: [] };
         board.lanes = [...board.lanes, newLane];
-    }
-
+    };
 </script>
+
 <div class="board">
-{#each board.lanes as lane}
-<div role="group" on:drop={(e) => onDrop(e, lane.id)} on:dragover={onDragOver} class="lane">
-    <h2>{lane.title}</h2>
-    <div class="items">
-    {#each lane.items as item}
-    <div role="listitem" on:dragstart={(e) => onDragStart(e,item.id, lane.id)} draggable=true class="item">
-        <p>{item.title}</p>
-    </div>
+    {#each board.lanes as lane}
+        <div
+            role="group"
+            on:drop={(e) => onDrop(e, lane.id)}
+            on:dragover={onDragOver}
+            class="lane"
+        >
+            <h2>{lane.title}</h2>
+            <div class="items">
+                {#each lane.items as item}
+                    <div
+                        role="listitem"
+                        on:dragstart={(e) => onDragStart(e, item.id, lane.id)}
+                        draggable="true"
+                        class="item"
+                    >
+                        <p>{item.title}</p>
+                    </div>
+                {/each}
+            </div>
+        </div>
     {/each}
-    </div>
-</div>
-{/each}
     <div class="add-lane">
         <button on:click={() => addLane()}>+</button>
     </div>
 </div>
+
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
-    :global(*){
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap");
+
+    :global(*) {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
+        font-family: "Poppins", sans-serif;
         color: #fff;
     }
-    :global(body){
+
+    :global(body) {
         background-color: #111;
     }
-    
-    .board{
+
+    .board {
         position: relative;
         padding: 2rem;
         display: grid;
@@ -90,28 +105,33 @@
         height: 100%;
         grid-template-columns: repeat(auto-fill, minmax(14rem, 1fr));
     }
-    .lane{
+
+    .lane {
         border-radius: 1rem;
         padding: 1rem;
         background-color: #222;
     }
-    .items{
+
+    .items {
         margin-top: 2rem;
         display: grid;
         gap: 1rem;
     }
-    .item{
+
+    .item {
         min-height: 6rem;
         border-radius: 1rem;
         padding: 1rem;
         background-color: #333;
         cursor: move;
     }
-    .add-lane{
+
+    .add-lane {
         display: grid;
         place-content: center;
     }
-    .add-lane button{
+
+    .add-lane button {
         all: unset;
         font-weight: 900;
         font-size: 4rem;
