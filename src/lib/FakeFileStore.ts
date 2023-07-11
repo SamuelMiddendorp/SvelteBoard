292a8a1c3bb1988da2c4acc0f70eb600cc4967d0
Samuel Middendorp <samiezaurus@gmail.com>
2023-07-11 13:01:17 +0200

    Install uuid types and assign id's to lanes and items in fakeApi store

--- src/lib/FakeFileStore.ts
+++ src/lib/FakeFileStore.ts
@@ -1,20 +1,24 @@
 import type { Board } from "./model";
+import { v4 as uuidv4 } from 'uuid';
 
 export const getBoard = () : Board => {
     return {
+        id: uuidv4(),
         title: "First Board",
         description: "My first board",
         lanes: [{
+            id: uuidv4(),
             title: "My first lane",
             items: [
-                {title: "foo", description: "bar", prio: 1},
-                {title: "foo", description: "bar", prio: 1},
-                {title: "foo", description: "bar", prio: 1},
-                {title: "foo", description: "bar", prio: 1},
-                {title: "foo", description: "bar", prio: 1},
+                {id: uuidv4(), title: "foo", description: "bar", prio: 1},
+                {id: uuidv4(), title: "foo", description: "bar", prio: 1},
+                {id: uuidv4(), title: "foo", description: "bar", prio: 1},
+                {id: uuidv4(), title: "foo", description: "bar", prio: 1},
+                {id: uuidv4(), title: "foo", description: "bar", prio: 1},
             ]
         },
         {
+            id: uuidv4(),
             title: "My second lane",
             items: []
         },
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
     import { getBoard } from "$lib/FakeFileStore";
     import type { Board, Item } from "$lib/model";
-    let internalIds : {[id: string] : Item;} = {};
 
     let board : Board = getBoard();
     let onDrop = (e: any) =>{
-        let data = e.dataTransfer.getData("text");
-        e.target.appendChild(document.getElementById(data));
+        let itemId = e.dataTransfer.getData("itemId");
+        let originalLaneId = e.dataTransfer.getData("laneId");
     }
     let onDragOver = (e: any) =>{
         e.preventDefault();
     }
-    let onDragStart = (e: any, id: number) => {
-        e.dataTransfer.setData("itemId", id);
+    let onDragStart = (e: any, itemId: number, laneId: number) => {
+        e.dataTransfer.setData("itemId", itemId);
+        e.dataTransfer.setData("laneId", laneId);
     }
 
 </script>
@@ -21,7 +21,7 @@
 <div on:drop={onDrop} on:dragover={onDragOver} class="lane">
     <h2>{lane.title}</h2>
     {#each lane.items as item,id}
-    <div on:dragstart={(e) => onDragStart(e,id)} draggable=true class="item">
+    <div on:dragstart={(e) => onDragStart(e,item.id, lanel.id)} draggable=true class="item">
         <p>{item.title}</p>
     </div>
     {/each}
